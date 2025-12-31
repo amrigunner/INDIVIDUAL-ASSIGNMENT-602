03b08e8f7bc884610e62bf35f6b4c2d4dc8f2a3d
amrig <amrigunner@gmail.com>
2026-01-01 04:45:12 +0800

    Initial commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://gxeahicokyygyxlutcvo.supabase.co',
+    supabaseKey:
+     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd4ZWFoaWNva3l5Z3l4bHV0Y3ZvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjcxMTYyNTgsImV4cCI6MjA4MjY5MjI1OH0.xkMKwNVj9VU5nEmvVkYUOqYrC6o-ijdJP4Zg6PyuzFc',
+};
